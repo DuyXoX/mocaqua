df1adad634aca1892439cba6c50893183dd3c510
Minh Duy <vminhduy615@gmail.com>
2023-05-29 19:49:25 +0700

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  return res.send("Hello web");
+  return res.send("Hello web mocaqua");
 });
 
 app.listen(port, hostname, () => {
